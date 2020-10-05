2c30f44ed4384c136b53f04fe1331ce6769442a0
anuragpurkayastha <51949431+anuragpurkayastha@users.noreply.github.com>
2020-10-06 09:58:23 +1100

    Update greeting.js

--- js/greeting.js
+++ js/greeting.js
@@ -6,8 +6,8 @@ var Hr = today.getHours();
 var name = 'Anurag';
 
 // Here you can change your greetings
-var gree1 = 'Go to Sleep! ';
-var gree2 = 'Good morning! ';
+var gree1 = 'Go to sleep ';
+var gree2 = 'Good morning ';
 var gree3 = 'Good afternoon ';
 var gree4 = 'Good evening ';
 var gree5 = 'Good evening ';
@@ -15,11 +15,11 @@ var gree6 = 'Good evening ';
 
 // Define the hours of the greetings
 if (Hr >= 23 && Hr < 5) {
-    document.getElementById('greetings').innerText = gree1 + name;
+    document.getElementById('greetings').innerText = gree1 + name + '!';
 } else if (Hr >= 6 && Hr < 12) {
-    document.getElementById('greetings').innerText = gree2 + name;
+    document.getElementById('greetings').innerText = gree2 + name + '!';
 } else if (Hr >= 12 && Hr < 17) {
-    document.getElementById('greetings').innerText = gree3 + name;
+    document.getElementById('greetings').innerText = gree3 + name + '!';
 } else if (Hr >= 17 && Hr < 23) {
-    document.getElementById('greetings').innerText = gree4 + name;
+    document.getElementById('greetings').innerText = gree4 + name + '!';
 }
